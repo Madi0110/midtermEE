0f03fc6e2f0125cb22cf1af06cb06227b80f5efb
Madi <63842217+Madi0110@users.noreply.github.com>
2021-04-25 06:11:30 +0600

    2

--- src/main/java/com/company/servlets/NewsServlet.java
+++ src/main/java/com/company/servlets/NewsServlet.java
@@ -1,7 +1,9 @@
 package com.company.servlets;
 
 import com.company.database.News;
+import com.company.database.User;
 import com.company.entities.NewsEntity;
+import com.company.entities.UserEntity;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,12 +24,12 @@ public class NewsServlet extends BaseServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         News newsDao = new News();
-
+        UserEntity userEntity = (UserEntity) req.getSession().getAttribute("user");
         newsDao.addNews(new NewsEntity(
                 req.getParameter("title"),
                 req.getParameter("body"),
-                (int)req.getSession().getAttribute("id")
-        ));
+                userEntity.getId()
+                ));
 
         req.getRequestDispatcher("add-news.jsp").forward(req, resp);
     }
